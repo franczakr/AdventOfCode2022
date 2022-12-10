ed489b2637d85c31824f6dfb64ea2abf0d638f61
Rafał Franczak <franczakr066@gmail.com>
2022-12-10 01:37:52 +0100

    Day 9

---
+++ src/main/kotlin/day9/Day9.kt
@@ -0,0 +1,89 @@
+@file:Suppress("ComplexRedundantLet")
+
+package day9
+
+import util.readLines
+import java.util.*
+import java.util.stream.IntStream
+import kotlin.math.abs
+import kotlin.math.max
+
+fun main() {
+
+    val testLines = readLines("day9/input_test.txt")
+    part1(testLines).let { check(it == 13) }
+    part2(testLines).let { check(it == 1) }
+
+    val lines = readLines("day9/input.txt")
+    part1(lines).let { println("[Part 1] $it") }
+    part2(lines).let { println("[Part 2] $it") }
+}
+
+class Node(var x: Int = 0, var y: Int = 0) {
+    fun distanceTo(node: Node): Int = max(abs(x - node.x), abs(y - node.y))
+
+    fun move(direction: String) {
+        when (direction) {
+            "U" -> y += 1
+            "D" -> y -= 1
+            "R" -> x += 1
+            "L" -> x -= 1
+            else -> error("Invalid direction")
+        }
+    }
+
+    fun moveTo(node: Node) {
+        when {
+            x > node.x -> x -= 1
+            x < node.x -> x += 1
+        }
+        when {
+            y > node.y -> y -= 1
+            y < node.y -> y += 1
+        }
+    }
+}
+
+// part 1
+private fun part1(lines: List<String>): Int {
+    val head = Node(0, 0)
+    val tail = Node(0, 0)
+
+    val visited: MutableSet<Pair<Int, Int>> = mutableSetOf()
+
+    lines.forEach { line ->
+        val (direction, count) = line.split(" ")
+
+        for (i in 1..count.toInt()) {
+            head.move(direction)
+            if (tail.distanceTo(head) > 1) {
+                tail.moveTo(head)
+            }
+            visited.add(Pair(tail.x, tail.y))
+        }
+    }
+    return visited.size
+}
+
+// part 2
+private fun part2(lines: List<String>): Int {
+    val rope = IntStream.rangeClosed(1, 10).mapToObj { Node(0, 0) }.toList()
+
+    val visited: MutableSet<Pair<Int, Int>> = mutableSetOf()
+
+    lines.forEach { line ->
+        val (direction, count) = line.split(" ")
+
+        for (i in 1..count.toInt()) {
+            rope[0].move(direction)
+            for (n in 1..rope.lastIndex) {
+                if (rope[n].distanceTo(rope[n - 1]) > 1) {
+                    rope[n].moveTo(rope[n - 1])
+                }
+            }
+            visited.add(rope[rope.lastIndex].let { Pair(it.x, it.y) })
+        }
+    }
+    return visited.size
+}
+
